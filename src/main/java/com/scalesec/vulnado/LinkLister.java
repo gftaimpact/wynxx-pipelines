3689b20d132faa752eedd797228eaee61066f779
Julio Arruda <julio.arruda@outlook.com>
2026-06-10 14:38:12 -0400

    Fix error message for private IP check

--- src/main/java/com/scalesec/vulnado/LinkLister.java
+++ src/main/java/com/scalesec/vulnado/LinkLister.java
@@ -27,7 +27,7 @@ public class LinkLister {
       String host = aUrl.getHost();
       System.out.println(host);
       if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")){
-        throw new BadRequest("Use of Private IP");
+        throw new BadRequest("Use of Private IPsaa");
       } else {
         return getLinks(url);
       }
