39a919989128618304e77280f4e5ac8a480fef71
Julio Arruda <julio.arruda@outlook.com>
2026-06-09 22:12:49 -0400

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/LinkLister.java

--- src/main/java/com/scalesec/vulnado/LinkLister.java
+++ src/main/java/com/scalesec/vulnado/LinkLister.java
@@ -1,38 +1,5 @@
-package com.scalesec.vulnado;
+I notice that the REMARKS section is empty - there are no specific issues reported by the SAST tool for this code. Since there are no remarks to address, no changes are needed.
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.IOException;
-import java.net.*;
+However, I can see this code has some syntax issues (likely due to formatting/special characters being stripped), but since no specific remarks were provided, I will not make any changes to avoid introducing unintended modifications.
 
-
-public class LinkLister {
-  public static List<String> getLinks(String url) throws IOException {
-    List<String> result = new ArrayList<String>();
-    Document doc = Jsoup.connect(url).get();
-    Elements links = doc.select("a");
-    for (Element link : links) {
-      result.add(link.absUrl("href"));
-    }
-    return result;
-  }
-
-  public static List<String> getLinksV2(String url) throws BadRequest {
-    try {
-      URL aUrl= new URL(url);
-      String host = aUrl.getHost();
-      System.out.println(host);
-      if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")){
-        throw new BadRequest("Use of Private IP");
-      } else {
-        return getLinks(url);
-      }
-    } catch(Exception e) {
-      throw new BadRequest(e.getMessage());
-    }
-  }
-}
+If you have specific remarks or issues to address, please provide them and I'll be happy to fix the code accordingly.
